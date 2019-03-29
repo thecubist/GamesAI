f4703564a6d84ec6bb0e56f0617037e720795c78
Stefan <36249159+thecubist@users.noreply.github.com>
2019-03-29 15:48:17 +0000

    added more procedural generation code with perlin noise

--- Math_And_AI_Assignment/Assets/scripts/ProceduralGeneration.cs
+++ Math_And_AI_Assignment/Assets/scripts/ProceduralGeneration.cs
@@ -6,14 +6,18 @@ public class ProceduralGeneration : MonoBehaviour
 {
     public bool simpleInstance = true;
     public Vector3 tileCount = new Vector3(1, 1, 1);
-    public Vector3 tilePosMult = new Vector3(0,0,0);
-    public GameObject mesh;
-    public GameObject[] tileMeshes = new GameObject[1]; 
+    public Vector3 tilePosMult = new Vector3(0, 0, 0);
+    public GameObject[] tileMeshes = new GameObject[1];
+    public Vector2 perlinMultipliers = new Vector2(0.005f, 0.005f);
+    private bool generateTileState = false;
+    void Start()
+    {
+        generateTiles();
+    }
 
-	void Start()
+    void generateTiles()
     {
         Vector3 instancePos; //used for defining where the next mesh is generated
-        float tempY;
 
         if (tileCount.x < 1 || tileCount.y < 1 || tileCount.z < 1)
         {
@@ -28,6 +32,7 @@ public class ProceduralGeneration : MonoBehaviour
         }
 
         //int meshCount = (int)tileCount.x * (int)tileCount.y * (int)tileCount.z;
+        int meshID = 0;
 
         //generate the tiles
         for (int i = 0; i < tileCount.x; i++)
@@ -38,26 +43,41 @@ public class ProceduralGeneration : MonoBehaviour
                 {
                     if (simpleInstance)
                     {
-                        tempY = Mathf.PerlinNoise(i, k);
-
                         instancePos = new Vector3(i * tilePosMult.x, j * tilePosMult.y, k * tilePosMult.z);
-                        Instantiate(mesh, instancePos, Quaternion.identity);
-                        Debug.Log(instancePos);
+                        Instantiate(tileMeshes[0], instancePos, Quaternion.identity);
                     }
                     else
                     {
+                        //meshID = Random.Range(0, tileMeshes.Length);
 
+
+                        if (Mathf.PerlinNoise(Random.Range(0, i) * perlinMultipliers.x, Random.Range(0, j) * perlinMultipliers.y) > 0.5)
+                            meshID = 1;
+                        else
+                            meshID = 0;
+
+                        //Debug.Log("mesh is " + meshID);
+                        //Debug.Log("perlin is " + Mathf.PerlinNoise(i * 0.005f, j * 0.005f));
+                        instancePos = new Vector3(i * tilePosMult.x, j * tilePosMult.y, k * tilePosMult.z);
+                        Instantiate(tileMeshes[meshID], instancePos, Quaternion.identity);
                     }
                 }
             }
         }
-	}
+    }
 
-
-	
-	// Update is called once per frame
-	void Update ()
+    // Update is called once per frame
+    void Update()
     {
-		
-	}
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            GameObject[] allObjects = GameObject.FindGameObjectsWithTag("ProceduralGenerationTile");
+            foreach (GameObject obj in allObjects)
+            {
+                Destroy(obj);
+            }
+
+            generateTiles();
+        }
+    }
 }
